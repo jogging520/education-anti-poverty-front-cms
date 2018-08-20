a7e92f8dbeccfe14b76d0275315e33a4f7afe2aa
446734796@qq.com <wuqiang502>
2018-08-20 18:05:42 +0800

    增加了dashboard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  serverUrl:         `http://223.105.5.116:9090/`,
+  serverUrl:          `http://223.105.5.116:9090/`,
+  webSocketUrl:       `ws://223.105.5.116:9090/`,
   production:         false,
   useHash:            true,
   hmr:                false,
